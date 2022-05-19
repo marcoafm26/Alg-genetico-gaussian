13372b8fccf80fe735300b8f5b369b4cb4647781
marcoafm26 <marcoaureliofm26@gmail.com>
2022-05-19 19:30:37 -0300

    new instance implementation ia.v4

--- src/domain/impl/IndividuoCrossInTray.java
+++ src/domain/impl/IndividuoCrossInTray.java
@@ -4,44 +4,48 @@ import domain.IndividuoRepresentacaoReal;
 import domain.Individuo;
 import java.util.*;
 
-public class IndividuoCrossInTray extends IndividuoRepresentacaoReal {
+public class IndividuoPermFunction extends IndividuoRepresentacaoReal {
 
 
-
-    public IndividuoCrossInTray(int nRainhas) {
-        nDimension = nRainhas;
-        random = new Random();
-        genes = super.geraGenes(this.nDimension);
+    @Override
+    public Individuo getNewInstance(List<Double> genes) {
+        return new IndividuoPermFunction(nDimension, domain_x, domain_y, chanceMutacao,genes);
     }
 
-    public IndividuoCrossInTray(int nRainhas, List<Double> genes) {
-        this.nDimension = nRainhas;
-        random = new Random();
-        this.genes = genes;
+    public IndividuoPermFunction(int nRainhas, double domain_x, double domain_y, double chanceMutacao) {
+        super(nRainhas, domain_x, domain_y, chanceMutacao);
+    }
+
+    public IndividuoPermFunction(int nRainhas, double domain_x, double domain_y, double chanceMutacao,List<Double> genes) {
+        super(nRainhas, domain_x, domain_y, chanceMutacao,genes);
     }
 
     //TODO implementar os métodos da classe
     // numero de colisoes: diagonal
     @Override
     public Double avaliar() {
-        return avaliacao;
+        double sum =0;
+
+        for (int i = 1; i <= nDimension; i++) {
+            double value = 0;
+            for (int j = 1; j <= this.nDimension; j++) {
+                double inner1 = Math.pow(j, i)+ 0.5;
+                double inner2 = Math.pow(this.genes.get(j-1)/j,i) - 1;
+                value = inner1 * inner2 + value;
+            }
+            sum += Math.pow(value,2);
+        }
+        this.avaliacao = sum;
+        return this.avaliacao;
     }
 
-    
-
-    public static void main(String[] args) {
-        //List<Integer> genes = Arrays.asList(0, 1, 2, 3, 4, 5, 6 ,7);
-        List<Integer> genes = Arrays.asList(5, 2, 7, 0, 3, 6, 4 ,1);
-        //Individuo individuo = new IndividuoPermFunction(8,genes);
-       // System.out.println(individuo.getAvaliacao());
-    }
 
     @Override
     public List<Individuo> getFilhos(Individuo ind) {
         return super.getFilhos(ind);
     }
 
-    @Override
+       @Override
     public Individuo getMutante() {
         return super.getMutante();
     }
@@ -51,9 +55,10 @@ public class IndividuoCrossInTray extends IndividuoRepresentacaoReal {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
-    public int compareTo(Individuo o) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public static void main(String[] args) {
+        //List<Integer> genes = Arrays.asList(0, 1, 2, 3, 4, 5, 6 ,7);
+        List<Double> genes = Arrays.asList(1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0);
+        Individuo individuo = new IndividuoPermFunction(8,-8,8,0.1,genes);
+        System.out.println(individuo.getAvaliacao());
     }
-
 }
